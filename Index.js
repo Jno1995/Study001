c4d35f2315ff4c2d8f46754b1752e7bd142c7576
Jno <35944775+Jno1995@users.noreply.github.com>
2019-08-07 14:26:47 +0800

    Initial commit

---
+++ Index.js
@@ -0,0 +1,11 @@
+var server = require("./Server");
+var router = require("./Router");
+var requestHandlers = require("./RequestHandlers");
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+server.start(router.route, handle);
